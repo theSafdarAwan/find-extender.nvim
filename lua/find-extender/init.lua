61512bea44ad5a7ac947566b940b4f6a24780d44
TheSafdarAwan <thesafdarawan@gmail.com>
2023-02-23 15:59:21 +0500

    bug(fix): fixed the error caused by not providing a tbl to setup function
    If a table wasn't provided to the setup function then it caused error
    when trying to merge the default_config and the user config

--- lua/find-extender/init.lua
+++ lua/find-extender/init.lua
@@ -6,7 +6,7 @@ function M.setup(user_config)
 		start_timeout_after_chars = 2,
 	}
 	-- merge the user config and the default config
-	local config = vim.tbl_extend("force", default_config, user_config)
+	local config = vim.tbl_extend("force", default_config, user_config or {})
 
 	require("find-extender.finder").finder(config)
 end
